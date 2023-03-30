17de49e3d5a14408799db7dd19b357fa0c69165c
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-30 22:45:31 +0200

    end

---
+++ database/seeders/trainsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class trainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $new_train = new Train;
+
+        for ($i=0; $i < 100; $i++) {
+            $new_train->company = $faker->randomElement(["Trenitalia", "Frecciarossa", "Italo"]);
+            $new_train->departure_station = randomElement(["Reggio Calabria Centrale", "Milano Centrale", "Roma Termini"]);
+            $new_train->arrival_station = randomElement(["Reggio Calabria Centrale", "Milano Centrale", "Roma Termini"]);
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = $faker->numerify("Treno-numero-###");
+            $new_train->carriages = rand(2, 7);
+            $new_train->on_time = $faker->boolean(60);
+            $new_train->cancelled = $faker->boolean(10);
+            
+            $new_train->save();
+        }
+
+    }
+}
